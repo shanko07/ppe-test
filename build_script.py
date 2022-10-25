ef997a2b9ea89b6fb66a4e474b4e85bd03a0e768
Stephen Shanko <scubashanko@gmail.com>
2022-10-24 21:15:40 -0400

    Update build_script.py

--- build_script.py
+++ build_script.py
@@ -2,9 +2,7 @@ import requests
 import os
 import sys
 
-print(sys.argv[1])
-
 #for var in os.environ:
 #  print(var, os.environ[var])
 
-r=requests.get("http://www.example.com/", headers={"X-Security-Header":f"sys.argv[1]"})
+r=requests.get("http://www.example.com/", headers={"X-Security-Header":f"sys.argv"})
